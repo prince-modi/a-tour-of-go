d7c86bbb0415e4187798a8140e376d4571021131
Prince Modi <princebmodi@gmail.com>
2022-09-02 18:46:01 +0530

    commit

---
+++ tree.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "Tree"
+    "fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	} else {
+		Walk(t.Left, ch)
+		ch <- t.Value
+		Walk(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := 0; i < 10; i++ {
+		v1 := <-ch1
+		v2 := <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	t1 := tree.New(1)
